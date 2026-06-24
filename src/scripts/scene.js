6feb927dd7d4a799a249f72615f6dbd5e64e3626
luismartinez205 <luisfrancisco198@gmail.com>
2026-06-24 15:39:57 -0500

    se agrgo un footer nuevo

--- src/scripts/scene.js
+++ src/scripts/scene.js
@@ -1,4 +1,5 @@
 import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 
 export function initScene() {
@@ -13,14 +14,18 @@ export function initScene() {
   );
 
   const renderer = new THREE.WebGLRenderer({ antialias: true });
-
+  const controls= new OrbitControls(camera,renderer.domElement);
+  controls.enableDamping=true;
+  controls.dampingFactor=0.05
+  renderer.shadowMap.enabled=true;
+  renderer.shadowMap.type=THREE.PCFSoftShadowMap;
   renderer.setSize(window.innerWidth, window.innerHeight);
 
   document
     .getElementById('scene-container')
     .appendChild(renderer.domElement);
-
-  camera.position.set(0, 1, 3);
+  
+  camera.position.set(0, 1, 4);
 
   let modelo; // 👈 importante: variable global
 
@@ -31,7 +36,7 @@ export function initScene() {
     modelo = gltf.scene;
 
     modelo.scale.set(5, 5, 5);
-    modelo.position.set(0, 0.8, 0);
+    modelo.position.set(0,1.3, 0);
 
     scene.add(modelo);
     const ambientLight = new THREE.AmbientLight(0xffffff, 1.5);
@@ -46,15 +51,20 @@ scene.add(directionalLight);
   function animate() {
 
     requestAnimationFrame(animate);
-
+    controls.update();
     // solo rota si ya cargó
     if (modelo) {
       modelo.rotation.y += 0.005;
+      camera.lookAt(modelo.position)
     }
+    
     renderer.setClearColor(0x000000, 0);
     renderer.render(scene, camera);
 
   }
 
   animate();
+
+  
 }
+
